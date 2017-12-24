0ab3505ccbefabe6eb686ad0c98301cec8936de0
Michelle Hamdani <michellehamdani@gmail.com>
2017-12-23 18:54:28 -0800

    PORT update

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require('path');
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
+
+// Scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
+
 // Require all models
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 // Initialize Express
 var app = express();
+
 // Configure middleware
-// Use morgan logger for logging requests
 app.use(logger("dev"));
+
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: false }));
 // Use express.static to serve the public folder as a static directory
@@ -88,7 +90,6 @@ app.get("/articles/:id", function(req, res) {
     // ..and populate all of the notes associated with it
     .populate("note")
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
